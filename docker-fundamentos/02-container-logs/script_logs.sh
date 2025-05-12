d918534139924b4f1453129d2ce4b2020eab8b00
Jardson <136630558+peresl@users.noreply.github.com>
2025-05-12 09:08:55 -0300

    Update

--- docker-fundamentos/02-container-logs/script_logs.sh
+++ docker-fundamentos/02-container-logs/script_logs.sh
@@ -1,8 +1,8 @@
-docker volume create nginx_marknini
-docker run -d --name meu-nginx -p 8080:80 -v nginx_marknini:/var/log/nginx nginx
+docker volume create nginx_jardson
+docker run -d --name meu-nginx -p 8080:80 -v nginx_jardson:/var/log/nginx nginx
 curl http://localhost:8080
 docker stop meu-nginx
 docker rm meu-nginx
 
 # Rodar novamente e verificar se os logs estão lá
-docker run -it --rm -v nginx_marknini:/var/log/nginx alpine ls /var/log/nginx
+docker run -it --rm -v nginx_jardson:/var/log/nginx alpine ls /var/log/nginx
